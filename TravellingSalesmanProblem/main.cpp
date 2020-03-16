4bfa00a5e3cb465b6b5011356d18695b79c7d046
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2020-03-16 12:10:07 +0100

    Add random init parents population

--- TravellingSalesmanProblem/main.cpp
+++ TravellingSalesmanProblem/main.cpp
@@ -2,10 +2,14 @@
 #include <array>
 #include <algorithm>
 #include <numeric>
+#include <random>
+
+// y = [1, 4, 5, 3, 0, 4, 10, 6, 9, 10]
 
 // Cities 1 ((2 + 3 + 0 + 9 + 1 + 9 + 9 + 7) % 5 = 0)
 // x = [0, 3, 6, 7, 15, 12, 14, 9, 7, 0]
-// y = [1, 4, 5, 3, 0, 4, 10, 6, 9, 10]
+
+typedef std::array<uint32_t, 10> array;
 
 template<typename T, std::size_t N>
 std::ostream& operator<<(std::ostream& stream, std::array<T, N>& array) {
@@ -27,17 +31,28 @@ std::ostream& operator<<(std::ostream& stream, const TT<T...>& collection) {
     return stream;
 }
 
-typedef std::array<uint32_t, 10> array;
+//TODO: random init population template<class T, std::size_t size>
 
 int main() {
-    std::size_t noParents = 250;
-    double n = 0.8; // need a better name for this variable
-    double pm = 0.2; // need a better name for this variable
-    std::size_t tMax = 1000;
+    const std::size_t noParents = 250;
+    const double n = 0.8; // need a better name for this variable
+    const double pm = 0.2; // need a better name for this variable
+    const std::size_t tMax = 1000;
 
+    array x = {0, 3, 6, 7, 15, 12, 14, 9, 7, 0};
+    array y = {1, 4, 5, 3, 0, 4, 10, 6, 9, 10};
     array cities;
     std::iota(cities.begin(), cities.end(), 1);
-    std::cout << cities;
+    std::cout << cities << std::endl << x << std::endl << y << std::endl;
+
+    std::random_device randomDevice;
+    std::mt19937 randomNumberGenerator(randomDevice());
+    std::array<array, noParents> population = {};
+    for (auto& parent : population) {
+        parent = cities;
+        std::shuffle(parent.begin(), parent.end(), randomNumberGenerator);
+        std::cout << parent << std::endl;
+    }
 
     return 0;
 }
