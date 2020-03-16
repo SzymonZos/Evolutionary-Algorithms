2fa8cdab9c2c96f1cc370b41514feb3d7268cb8b
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2020-03-16 10:50:28 +0100

    Add initial parameters

--- TravellingSalesmanProblem/main.cpp
+++ TravellingSalesmanProblem/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+// Cities 1 ((2 + 3 + 0 + 9 + 1 + 9 + 9 + 7) % 5 = 0)
+// x = [0, 3, 6, 7, 15, 12, 14, 9, 7, 0]
+// y = [1, 4, 5, 3, 0, 4, 10, 6, 9, 10]
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::size_t noParents = 250;
+    double n = 0.8; // need a better name for this variable
+    double pm = 0.2; // need a better name for this variable
+    std::size_t tMax = 1000;
+    
     return 0;
 }
