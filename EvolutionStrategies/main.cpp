aee3399389aa22bedac68d181bd5e053a7dc3a78
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2020-04-11 16:35:32 +0200

    Refactor evolution strategies algorithm

--- EvolutionStrategies/main.cpp
+++ EvolutionStrategies/main.cpp
@@ -30,114 +30,108 @@ auto GetModelFromFile(std::size_t id) {
     return model;
 }
 
-// double MeanSquaredError(const std::array<std::vector<double>, 2>& model,
-//                        const Chromosome& chromosome) {
-//    double meanSquaredError = 0.0;
-//    for (std::size_t i = 0; i < model.front().size(); i++) {
-//        double error = chromosome(model[Model::input][i]) -
-//                       model[Model::output][i];
-//        meanSquaredError += (error * error);
-//    }
-//    //    std::cout << meanSquaredError << std::endl;
-//    return meanSquaredError / static_cast<double>(model.front().size());
-//}
-//
-// void MutateChild(Chromosome& child) {
-//    using params = std::normal_distribution<double>::param_type;
-//    std::random_device randomDevice{};
-//    std::mt19937 rng{randomDevice()};
-//    std::normal_distribution childDistribution;
-//    std::normal_distribution standardDistribution{0.0, 1.0};
-//    const double n = 3.0;
-//    const double tau1 = 1.0 / std::sqrt(2.0 * n);
-//    const double tau2 = 1.0 / std::sqrt(2.0 * std::sqrt(n));
-//
-//    DblMatrix<2, 3> random;
-//    for (std::size_t i = 0; i < 3; i++) {
-//        random[i][0] = childDistribution(
-//            rng,
-//            params{0.0, child.standardDeviations_[i]});
-//        random[i][1] = std::exp(tau1 * standardDistribution(rng)) +
-//                       std::exp(tau2 * standardDistribution(rng));
-//    }
-//    child += random;
-//}
+template<std::size_t noCoefficients>
+double MeanSquaredError(const std::array<std::vector<double>, 2>& model,
+                        const ES::Chromosome<noCoefficients>& chromosome) {
+    double meanSquaredError = 0.0;
+    for (std::size_t i = 0; i < model.front().size(); i++) {
+        double error = chromosome(model[ES::in][i]) - model[ES::out][i];
+        meanSquaredError += (error * error);
+    }
+    return meanSquaredError / static_cast<double>(model.front().size());
+}
+
+template<std::size_t noCoefficients>
+void MutateChild(ES::Chromosome<noCoefficients>& child) {
+    using params = std::normal_distribution<double>::param_type;
+    std::random_device randomDevice{};
+    std::mt19937 rng{randomDevice()};
+    std::normal_distribution childDistribution;
+    std::normal_distribution standardDistribution{0.0, 1.0};
+    const double n = 3.0;
+    const double tau1 = 1.0 / std::sqrt(2.0 * n);
+    const double tau2 = 1.0 / std::sqrt(2.0 * std::sqrt(n));
+
+    ES::Chromosome<noCoefficients> random;
+    std::generate(random[ES::coeffs].begin(), random[ES::coeffs].end(), [&] {
+        static size_t i = 0;
+        return childDistribution(rng, params{0.0, child[ES::stddevs][i++]});
+    });
+    std::generate(random[ES::stddevs].begin(), random[ES::stddevs].end(), [&] {
+        return std::exp(tau1 * standardDistribution(rng)) +
+               std::exp(tau2 * standardDistribution(rng));
+    });
+    child += random;
+}
 
 int main() {
-    //    const auto model = GetModelFromFile(9);
-    //    const std::size_t noParents = model.front().size(); // mi
-    //    const std::size_t noOffspring = 6 * noParents; // lambda 5 - 7
-    //
-    //    std::vector<Chromosome> parents(noParents);
-    //    std::vector<double> parentsEvaluation(noParents);
-    //
-    //    // Initialize with uniform distribution
-    //    std::random_device randomDevice{};
-    //    std::mt19937 rng{randomDevice()};
-    //    std::uniform_real_distribution<>
-    //    coefficientsDistribution{-10.0, 10.0};
-    //    std::uniform_real_distribution<>
-    //    standardDeviationsDistribution{0.0, 10.0};
-    //    std::uniform_int_distribution<> parentsDistribution{
-    //        0,
-    //        static_cast<int>(noParents - 1)};
-    //
-    //    std::generate(parents.begin(), parents.end(), [&] {
-    //        DblMatrix<2, 3> random;
-    //        for (auto& params : random) {
-    //            params[0] = coefficientsDistribution(rng);
-    //            params[1] = standardDeviationsDistribution(rng);
-    //        }
-    //        return random;
-    //    });
-    //
-    //    std::generate(parentsEvaluation.begin(), parentsEvaluation.end(), [&]
-    //    {
-    //        static std::size_t i = 0;
-    //        return MeanSquaredError(model, parents[i++]);
-    //    });
-    //
-    //    //    std::cout << parentsEvaluation << std::endl;
-    //
-    //    std::vector<Chromosome> children(noOffspring);
-    //    std::generate(children.begin(), children.end(), [&] {
-    //        return
-    //        parents[static_cast<std::size_t>(parentsDistribution(rng))];
-    //    });
-    //
-    //    std::for_each(children.begin(), children.end(), MutateChild);
-    //
-    //    std::vector<double> childrenEvaluation(noParents);
-    //    std::generate(childrenEvaluation.begin(), childrenEvaluation.end(),
-    //    [&] {
-    //        static std::size_t i = 0;
-    //        return MeanSquaredError(model, children[i++]);
-    //    });
-    //
-    //    std::multimap<double, Chromosome> sortedPopulation{};
-    //    double minCostValue = 0.0;
-    //
-    //    for (std::size_t i = 0; i < noOffspring; i++) {
-    //        sortedPopulation.insert({childrenEvaluation[i], children[i]});
-    //        if (i < noParents) {
-    //            sortedPopulation.insert({parentsEvaluation[i], parents[i]});
-    //        }
-    //    }
-    //    auto it = sortedPopulation.begin();
-    //    std::generate(parents.begin(), parents.end(), [&] {
-    //        return (it++)->second;
-    //    });
-    //    minCostValue = sortedPopulation.begin()->first;
-    //
-    //    std::cout << sortedPopulation;
+    const auto model = GetModelFromFile(9);
+    const std::size_t noParents = model.front().size(); // mi
+    const std::size_t noOffspring = 6 * noParents; // lambda 5 - 7
+    constexpr std::size_t noCoefficients = 3;
+    using Chromosome = ES::Chromosome<noCoefficients>;
 
-    IntArray<3> test = {1, 2, 3};
-    IntArray<3> test2 = {6, 10, 20};
+    std::vector<Chromosome> parents(noParents);
+    std::vector<double> parentsEvaluation(noParents);
 
-    test += {2, 5, 6};
-    std::cout << test;
+    // Initialize with uniform distribution
+    std::random_device randomDevice{};
+    std::mt19937 rng{randomDevice()};
+    std::uniform_real_distribution<> coefficientsDistribution{-10.0, 10.0};
+    std::uniform_real_distribution<> standardDeviationsDistribution{0.0, 10.0};
+    std::uniform_int_distribution<> parentsDistribution{
+        0,
+        static_cast<int>(noParents - 1)};
 
-    ES::Chromosome<3>::Params duupa;
+    std::generate(parents.begin(), parents.end(), [&] {
+        ES::Chromosome<noCoefficients> random;
+        std::generate(random[ES::coeffs].begin(),
+                      random[ES::coeffs].end(),
+                      [&] { return coefficientsDistribution(rng); });
+        std::generate(random[ES::stddevs].begin(),
+                      random[ES::stddevs].end(),
+                      [&] { return standardDeviationsDistribution(rng); });
+        return random;
+    });
+
+    std::generate(parentsEvaluation.begin(), parentsEvaluation.end(), [&] {
+        static std::size_t i = 0;
+        return MeanSquaredError(model, parents[i++]);
+    });
+
+    //    std::cout << parentsEvaluation << std::endl;
+
+    std::vector<Chromosome> children(noOffspring);
+    std::generate(children.begin(), children.end(), [&] {
+        return parents[static_cast<std::size_t>(parentsDistribution(rng))];
+    });
+
+    std::for_each(children.begin(),
+                  children.end(),
+                  MutateChild<noCoefficients>);
+
+    std::vector<double> childrenEvaluation(noParents);
+    std::generate(childrenEvaluation.begin(), childrenEvaluation.end(), [&] {
+        static std::size_t i = 0;
+        return MeanSquaredError(model, children[i++]);
+    });
+
+    std::multimap<double, Chromosome> sortedPopulation{};
+    double minCostValue = 0.0;
+
+    for (std::size_t i = 0; i < noOffspring; i++) {
+        sortedPopulation.insert({childrenEvaluation[i], children[i]});
+        if (i < noParents) {
+            sortedPopulation.insert({parentsEvaluation[i], parents[i]});
+        }
+    }
+    auto it = sortedPopulation.begin();
+    std::generate(parents.begin(), parents.end(), [&] {
+        return (it++)->second;
+    });
+    minCostValue = sortedPopulation.begin()->first;
+
+    std::cout << sortedPopulation << minCostValue;
 
     return 0;
 }
