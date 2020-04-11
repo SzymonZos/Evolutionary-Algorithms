f17f69e43210c2f8e6bf407e23c1685d23205647
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2020-04-11 15:37:55 +0200

    Refactor Chromosome class, provide array operator overloads

--- EvolutionStrategies/include/Types.hpp
+++ EvolutionStrategies/include/Types.hpp
@@ -2,6 +2,62 @@
 #define EVOLUTIONSTRATEGIES_TYPES_HPP
 
 #include <array>
+#include <functional>
+
+template<typename BinaryOperation, typename T, std::size_t size>
+auto ElementWise(const std::array<T, size>& lhs,
+                 const std::array<T, size>& rhs,
+                 BinaryOperation operation) {
+    std::array<T, size> ret;
+    for (std::size_t i = 0; i < size; i++) {
+        ret[i] = std::invoke(operation, lhs[i], rhs[i]);
+    }
+    return ret;
+}
+
+template<typename T, std::size_t size>
+auto operator+(const std::array<T, size>& lhs,
+               const std::array<T, size>& rhs) {
+    return ElementWise(lhs, rhs, std::plus<T>());
+}
+
+template<typename T, std::size_t size>
+auto& operator+=(std::array<T, size>& lhs, const std::array<T, size>& rhs) {
+    return lhs = lhs + rhs;
+}
+
+template<typename T, std::size_t size>
+auto operator-(const std::array<T, size>& lhs,
+               const std::array<T, size>& rhs) {
+    return ElementWise(lhs, rhs, std::minus<T>());
+}
+
+template<typename T, std::size_t size>
+auto& operator-=(std::array<T, size>& lhs, const std::array<T, size>& rhs) {
+    return lhs = lhs - rhs;
+}
+
+template<typename T, std::size_t size>
+auto operator*(const std::array<T, size>& lhs,
+               const std::array<T, size>& rhs) {
+    return ElementWise(lhs, rhs, std::multiplies<T>());
+}
+
+template<typename T, std::size_t size>
+auto& operator*=(std::array<T, size>& lhs, const std::array<T, size>& rhs) {
+    return lhs = lhs * rhs;
+}
+
+template<typename T, std::size_t size>
+auto operator/(const std::array<T, size>& lhs,
+               const std::array<T, size>& rhs) {
+    return ElementWise(lhs, rhs, std::divides<T>());
+}
+
+template<typename T, std::size_t size>
+auto& operator/=(std::array<T, size>& lhs, const std::array<T, size>& rhs) {
+    return lhs = lhs / rhs;
+}
 
 template<std::size_t size>
 using IntArray = std::array<std::size_t, size>;
