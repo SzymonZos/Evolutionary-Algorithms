e60045c97835d26f167ed5d7c7b2b34df5cfcab7
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2022-02-16 18:48:32 +0100

    Add initial support for building apps in CI

---
+++ ci/config_compiler.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+case "$1" in
+  *gcc*)
+    CC="$(which gcc-11)" || CC="$(which gcc-10)"
+    CXX="$(which g++-11)" || CXX="$(which g++-10)"
+    ;;
+  *clang*)
+    sudo apt-get install libc++-12-dev libc++abi-12-dev
+    CC="$(which clang-12)" || CC="$(brew --prefix llvm)/bin/clang"
+    CXX="$(which clang++-12)" || CXX="$(brew --prefix llvm)/bin/clang++"
+    ;;
+esac
+echo "CC=$CC" >> "$GITHUB_ENV"
+echo "CXX=$CXX" >> "$GITHUB_ENV"
